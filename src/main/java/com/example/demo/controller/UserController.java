315bf83f3659d862cfaf14019aaa8103b1f11ebc
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 11:17:13 +0000

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
         // 실제 데이터베이스에서 사번 조회 로직
         //String empNo = "82107492"; // 예시: 실제로는 DB에서 조회해야 함
         try {
-                return ResponseEntity.ok(todoService.getSearchTodoList(id, keyword));
+                return ResponseEntity.ok("82107492"));
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
