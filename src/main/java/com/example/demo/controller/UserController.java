10b7f338f2babd32cd8934f34fcc05b1d1d3f71d
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 11:16:58 +0000

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,17 @@
 import org.springframework.web.bind.annotation.*;
 
-//@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/user")
 @RestController
 public class UserController {
 
-    @GetMapping("/api/v1/user")
-    public String getUserEmpNo() {
+    @GetMapping //("/api/v1/user")
+    public ResponseEntity<?> getUserEmpNo() {
         // 실제 데이터베이스에서 사번 조회 로직
         //String empNo = "82107492"; // 예시: 실제로는 DB에서 조회해야 함
-        return "82107492";
+        try {
+                return ResponseEntity.ok(todoService.getSearchTodoList(id, keyword));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
