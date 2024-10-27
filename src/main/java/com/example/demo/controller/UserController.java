a08af3f76caab2c62ff67d6445e9430ff50b3f12
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 22:45:28 +0900

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -16,6 +16,15 @@ public class UserController {
         // 실제 데이터베이스에서 사번 조회 로직
         //String empNo = "82107492"; // 예시: 실제로는 DB에서 조회해야 함
         try {
+                String[] info = new String[2];
+                info[0] = "Park";
+                info[1] = "82107492";
+                    
+			    List.of(info);
+			return ResponseEntity.ok(List.of(info).stream().map(f -> Map.of(
+					"name", info[0],
+					"path",	info[1]))
+                
                 return ResponseEntity.ok("82107492", "82107492");
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
