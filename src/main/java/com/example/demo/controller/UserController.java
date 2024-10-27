feaab1097d09f97bbfcb5dd16aab3ba28ecea769
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 22:23:26 +0900

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,3 +1,9 @@
+importimport lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/user")
@@ -9,7 +15,7 @@ public class UserController {
         // 실제 데이터베이스에서 사번 조회 로직
         //String empNo = "82107492"; // 예시: 실제로는 DB에서 조회해야 함
         try {
-                return ResponseEntity.ok("82107492"));
+                return ResponseEntity.ok("82107492");
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
