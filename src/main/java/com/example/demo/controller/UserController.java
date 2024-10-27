c1db25b344f4ea971102739d5b9cc9e822210503
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 22:40:48 +0900

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequestMapping("/api/v1/user")
 @RestController
 public class UserController {
@@ -15,7 +16,7 @@ public class UserController {
         // 실제 데이터베이스에서 사번 조회 로직
         //String empNo = "82107492"; // 예시: 실제로는 DB에서 조회해야 함
         try {
-                return ResponseEntity.ok("82107492");
+                return ResponseEntity.ok("82107492", "82107492");
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
