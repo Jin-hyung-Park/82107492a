50895eaeb9e65f067b781ddf632bb47d0da7696a
Jin-hyung-Park <45349175+Jin-hyung-Park@users.noreply.github.com>
2024-10-27 06:28:41 +0000

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,4 @@
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/user")
 @RestController
